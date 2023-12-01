71cb923b3b4bfba49160bf0fbf6c6807d728310e
Leegeonmin <kmlee97s@gmail.com>
2023-12-02 03:06:00 +0900

    feat : Entity 클래스 생성

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+// 상속관계에서 하나의 테이블로 합칠 때 사용
+@Inheritance(strategy =  InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+
+}
--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,13 +21,13 @@ public class MemberRepositoryTest {
     public void testMember() throws Exception{
         //given
         Member member = new Member();
-        member.setUsername("lee");
+        member.setName("lee");
         //when
         Long saveId = memberRepository.save(member);
         Member findMember = memberRepository.find(saveId);
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo((member.getUsername()));
+        Assertions.assertThat(findMember.getName()).isEqualTo((member.getName()));
         //같은 영속성 context에서는 id가 같으면 같은 객체로 취급한다
         Assertions.assertThat(findMember).isEqualTo((member));
     }
