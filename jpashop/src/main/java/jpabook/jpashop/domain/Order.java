7e6c29ed2f614cd7d663264994f4da6feafe659e
Leegeonmin <kmlee97s@gmail.com>
2024-01-09 04:33:27 +0900

    feat : 연관관계 설정 편의를 위한 메서드 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.data.util.Lazy;
 
 import java.time.LocalDateTime;
@@ -33,4 +34,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [Order, Cancel]
+
+
+    //==연관관계 메서드 ==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
