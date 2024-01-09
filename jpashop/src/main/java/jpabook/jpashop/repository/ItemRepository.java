56622f4238e7409381dd8aa538cc50427e50cf1e
Leegeonmin <kmlee97s@gmail.com>
2024-01-10 02:05:09 +0900

    feat : ItemRepository, ItemService 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    @PersistenceContext
+    private final EntityManager em;
+
+    //등록
+    public Long save(Item item){
+        if(item.getId() == null){
+            em.persist(item);
+        }else{
+            em.merge(item);
+        }
+        return item.getId();
+    }
+    // 단건 조회
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+    // 목록 조회
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
