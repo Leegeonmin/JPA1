6fcb5cefa633e0e360882d4cd9a549808fffb1c9
Leegeonmin <kmlee97s@gmail.com>
2024-01-11 06:10:57 +0900

    feat : 상품 검색을 위한 DTO OrderSearch 개발, criteria를 통한 동적 쿼리 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,10 +3,15 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -27,6 +32,29 @@ public class OrderRepository {
         return em.createQuery("select o from Order o ", Order.class)
                 .getResultList();
     }
-//    public List<Order> findAll(OrderSearch orderSearch)
 
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Order, Member> m = o.join("member", JoinType.INNER); //􀴥􀨗􀒗 􀪑􀩋
+        List<Predicate> criteria = new ArrayList<>();
+//􀪱􀞙 􀢚􀰕 􀑨􀢝
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"),
+                    orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+//􀴥􀨗 􀩉􀜴 􀑨􀢝
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName()
+                            + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //􀭭􀘀 1000
+
+        return query.getResultList();
+    }
 }
